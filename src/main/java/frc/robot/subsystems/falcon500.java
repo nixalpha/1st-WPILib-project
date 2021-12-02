047bbb9aa966c595ecfda2949da16d9ad53c479a
nixalpha <n1xalphaa@gmail.com>
2021-12-01 16:35:35 -0800

    Correct follower mode to control both talonfx motors
    according to @probablyanasian

--- src/main/java/frc/robot/subsystems/falcon500.java
+++ src/main/java/frc/robot/subsystems/falcon500.java
@@ -4,7 +4,7 @@
 
 package frc.robot.subsystems;
 
-// import com.ctre.phoenix.motorcontrol.TalonFXControlMode;
+import com.ctre.phoenix.motorcontrol.TalonFXControlMode;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
@@ -14,12 +14,13 @@ public class falcon500 extends SubsystemBase {
   private static WPI_TalonFX falcon2 = new WPI_TalonFX(3);
 
   public void setSpeed(double speed) {
-    falcon1.set(speed);
-    falcon2.set(speed);
+    falcon1.set(TalonFXControlMode.PercentOutput, speed);
   }
 
   /** Creates a new falcon500. */
-  public falcon500() {}
+  public falcon500() {
+    falcon2.set(TalonFXControlMode.Follower, 2);
+  }
 
   @Override
   public void periodic() {
